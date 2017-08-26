b5b43f2c81ff55ef09f7cf70b36cab40dda7c542
will_asiata <w.asiata@gmail.com>
2017-08-27 10:01:16 +1200

    Mocked up backend.
    Mocked up backend.

---
+++ src/routes/query.js
@@ -0,0 +1,50 @@
+var express = require('express');
+var request = require('request');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+
+var minLat = parseInt(req.query.minLat);
+var maxLat = parseInt(req.query.maxLat);
+var minLong = parseInt(req.query.minLong);
+var maxLong = parseInt(req.query.maxLong);
+
+// console.log(minLat + " " + maxLat);
+var halfLat = (minLat + maxLat) / 2;
+var halfLong = (minLong + maxLong) / 2;
+
+// console.log(halfLat);
+
+res.send(JSON.stringify({Coordinates: {Latitude: halfLat, Longitude: halfLong}}));
+
+
+
+// var str = "https://api.tmsandbox.co.nz/v1/Search/Property/Rental.JSON?latitude_max=" + maxLat;
+
+// var options = {
+//   url: str,
+//   headers: {
+//     'Authorization': 'OAuth oauth_consumer_key="A852C195374C7A2F70500DA81ED7357C",oauth_signature_method="HMAC-SHA1",oauth_timestamp="1503717980",oauth_nonce="75uMw4kz6mS",oauth_version="1.0",oauth_signature="zmyL3sLeSQiyMs40kyPh5ABbSfc%3D"'
+//   }
+// };
+//
+// console.log("a");
+//
+// function callback(error, response, body) {
+//   if (!error && response.statusCode == 200) {
+//     console.log("Not an error");
+//     var info = JSON.parse(body);
+//     console.log(info);
+//     res.send("We received the response");
+//   } else {
+//     console.log(error + response.statusCode);
+//     res.send('JSON stringify error');
+//   }
+// }
+//
+// request(options, callback);
+
+});
+
+module.exports = router;
